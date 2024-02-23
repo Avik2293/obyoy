a59ff5482a3f4562b7ca23dd7490e2076f2a0baf
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-02-23 11:25:50 +0600

    chore: change to tabs instead of spaces for indentation

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,172 +5,172 @@ import { IoArrowForwardCircleOutline } from "react-icons/io5";
 
 
 const Home = () => {
-    const [start, setStart] = useState(false);
-    const [input, setInput] = useState('')
-    const handleInputChange = (e) => setInput(e.target.value)
+	const [start, setStart] = useState(false);
+	const [input, setInput] = useState('')
+	const handleInputChange = (e) => setInput(e.target.value)
 
-    const handleSubmit = event => {
-        // event.preventDefault();
-        setInput('');
-        console.log(input);
-        setStart(!start);
-    };
+	const handleSubmit = event => {
+		// event.preventDefault();
+		setInput('');
+		console.log(input);
+		setStart(!start);
+	};
 
-    const handleSubmitNext = event => {
-        // event.preventDefault();
-        setInput('');
-        console.log(input);
-        // new line call 
-    };
+	const handleSubmitNext = event => {
+		// event.preventDefault();
+		setInput('');
+		console.log(input);
+		// new line call 
+	};
 
-    return (
-        <Container pb={{ base: '2', md: '4', }} px={{ base: '3', md: '8', }} maxWidth={"1400px"} mx={'auto'} >
-            <Grid gap={4} templateRows='repeat(3, 1fr)' templateColumns='repeat(5, 1fr)'
-            >
-                {/* left side part */}
-                <GridItem rowSpan={3} colSpan={[5, 5, 2, 2, 1]} bg='papayawhip' boxShadow='2xl' p='6' rounded='md' >
-                    <Text w="" fontSize="lg" fontWeight="bold" color='blue' textAlign={'center'} my={5}
-                    >Leaderboard (Balance)</Text>
+	return (
+		<Container pb={{ base: '2', md: '4', }} px={{ base: '3', md: '8', }} maxWidth={"1400px"} mx={'auto'} >
+			<Grid gap={4} templateRows='repeat(3, 1fr)' templateColumns='repeat(5, 1fr)'
+			>
+				{/* left side part */}
+				<GridItem rowSpan={3} colSpan={[5, 5, 2, 2, 1]} bg='papayawhip' boxShadow='2xl' p='6' rounded='md' >
+					<Text w="" fontSize="lg" fontWeight="bold" color='blue' textAlign={'center'} my={5}
+					>Leaderboard (Balance)</Text>
 
-                    <Text w="" fontSize="lg" fontWeight="bold" color='black' textAlign={'center'} my={5}
-                    >Top 5</Text>
+					<Text w="" fontSize="lg" fontWeight="bold" color='black' textAlign={'center'} my={5}
+					>Top 5</Text>
 
-                    {
-                        <>
-                            <Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
-                            >1. dummy (234423 tk)</Text>
-                            <Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
-                            >2. dummy (234423 tk)</Text>
-                            <Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
-                            >3. dummy (234423 tk)</Text>
-                            <Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
-                            >4. dummy (234423 tk)</Text>
-                            <Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
-                            >5. dummy (234423 tk)</Text>
-                        </>
-                    }
+					{
+						<>
+							<Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
+							>1. dummy (234423 tk)</Text>
+							<Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
+							>2. dummy (234423 tk)</Text>
+							<Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
+							>3. dummy (234423 tk)</Text>
+							<Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
+							>4. dummy (234423 tk)</Text>
+							<Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
+							>5. dummy (234423 tk)</Text>
+						</>
+					}
 
-                    <Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} my={5}
-                    >Next Top 5</Text>
+					<Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} my={5}
+					>Next Top 5</Text>
 
-                    {
-                        <>
-                            <Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
-                            >6. dummy (234423 tk)</Text>
-                            <Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
-                            >7. dummy (234423 tk)</Text>
-                            <Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
-                            >8. dummy (234423 tk)</Text>
-                            <Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
-                            >9. dummy (234423 tk)</Text>
-                            <Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
-                            >10. dummy (234423 tk)</Text>
-                        </>
-                    }
+					{
+						<>
+							<Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
+							>6. dummy (234423 tk)</Text>
+							<Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
+							>7. dummy (234423 tk)</Text>
+							<Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
+							>8. dummy (234423 tk)</Text>
+							<Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
+							>9. dummy (234423 tk)</Text>
+							<Text w="" fontSize="md" fontWeight="bold" color='black' textAlign={'center'}
+							>10. dummy (234423 tk)</Text>
+						</>
+					}
 
-                    <Text w="" fontSize="md" fontWeight="bold" color='blue' textAlign={'center'} mt={10} _hover={{ textDecoration: 'underline' }}>
-                        <ChakraLink as={ReactRouterLink} to='/leaderboard' >
-                            See Full List
-                        </ChakraLink>
-                    </Text>
-                </GridItem>
+					<Text w="" fontSize="md" fontWeight="bold" color='blue' textAlign={'center'} mt={10} _hover={{ textDecoration: 'underline' }}>
+						<ChakraLink as={ReactRouterLink} to='/leaderboard' >
+							See Full List
+						</ChakraLink>
+					</Text>
+				</GridItem>
 
-                {/* middle part */}
-                <GridItem rowSpan={3} colSpan={[5, 5, 3, 3, 3]} boxShadow='dark-lg' p='6' rounded='md' >
+				{/* middle part */}
+				<GridItem rowSpan={3} colSpan={[5, 5, 3, 3, 3]} boxShadow='dark-lg' p='6' rounded='md' >
 
-                    {
-                        !start &&
-                        <>
-                            <Text w="" fontSize="lg" fontWeight="bold" color='black' textAlign={'center'} mb={2}
-                            >Welcome,</Text>
-                            <Text w="" fontSize="lg" fontWeight="bold" color='black' textAlign={'center'} my={2}
-                            >Click "Start" to start translate.</Text>
+					{
+						!start &&
+						<>
+							<Text w="" fontSize="lg" fontWeight="bold" color='black' textAlign={'center'} mb={2}
+							>Welcome,</Text>
+							<Text w="" fontSize="lg" fontWeight="bold" color='black' textAlign={'center'} my={2}
+							>Click "Start" to start translate.</Text>
 
-                            <Text textAlign={'center'} my={2}>
-                                <Button rightIcon={<IoArrowForwardCircleOutline />} size="lg" bg={"blue"} color={"white"} _hover={{ bg: "blue.500" }} px={4} py={1} borderRadius={'lg'} onClick={() => setStart(!start)}
-                                >
-                                    Start
-                                </Button>
-                            </Text>
-                        </>
-                    }
+							<Text textAlign={'center'} my={2}>
+								<Button rightIcon={<IoArrowForwardCircleOutline />} size="lg" bg={"blue"} color={"white"} _hover={{ bg: "blue.500" }} px={4} py={1} borderRadius={'lg'} onClick={() => setStart(!start)}
+								>
+									Start
+								</Button>
+							</Text>
+						</>
+					}
 
-                    {
-                        start &&
-                        <Box>
-                            <Text w="" fontSize="lg" fontWeight="bold" color='black' textAlign={'center'} my={4}
-                            >Line 1</Text>
+					{
+						start &&
+						<Box>
+							<Text w="" fontSize="lg" fontWeight="bold" color='black' textAlign={'center'} my={4}
+							>Line 1</Text>
 
-                            <Text border='2px' borderColor='gray' borderRadius="md" fontSize="lg" fontWeight="semibold" bg={'gray'} color='white' textAlign={'center'} my={2} p={3} h={[null, '150px', '150px', '100px']}
-                            >.........................................
-                                ..........................................................
-                                ....................................................</Text>
+							<Text border='2px' borderColor='gray' borderRadius="md" fontSize="lg" fontWeight="semibold" bg={'gray'} color='white' textAlign={'center'} my={2} p={3} h={[null, '150px', '150px', '100px']}
+							>.........................................
+								..........................................................
+								....................................................</Text>
 
-                            {/* <Form onSubmit={handleSubmit}> */}
-                            <Form>
-                                <Input name="input" id="input" variant='filled' width='100%' p={3} my={3} placeholder='Type your input here..' onChange={handleInputChange} required value={input} h={[null, '150px', '150px', '100px']} />
+							{/* <Form onSubmit={handleSubmit}> */}
+							<Form>
+								<Input name="input" id="input" variant='filled' width='100%' p={3} my={3} placeholder='Type your input here..' onChange={handleInputChange} required value={input} h={[null, '150px', '150px', '100px']} />
 
-                                <HStack justify={'space-evenly'}>
-                                    <Text textAlign={'center'} my={2}>
-                                        <Button size="lg" bg={"blue"} color={"white"} _hover={{ bg: "blue.500" }} px={4} py={1} borderRadius={'lg'} type='submit' onClick={(event) => {
-                                            if (input) {
-                                                handleSubmit(event);
-                                            }
-                                        }}
-                                        >
-                                            Submit
-                                        </Button>
-                                    </Text>
+								<HStack justify={'space-evenly'}>
+									<Text textAlign={'center'} my={2}>
+										<Button size="lg" bg={"blue"} color={"white"} _hover={{ bg: "blue.500" }} px={4} py={1} borderRadius={'lg'} type='submit' onClick={(event) => {
+											if (input) {
+												handleSubmit(event);
+											}
+										}}
+										>
+											Submit
+										</Button>
+									</Text>
 
-                                    <Text textAlign={'end'} my={2}>
-                                        <Button size="lg" bg={"blue"} color={"white"} _hover={{ bg: "blue.500" }} px={4} py={1} borderRadius={'lg'} type='submit' onClick={(event) => {
-                                            if (input) {
-                                                handleSubmitNext(event);
-                                            }
-                                        }}
-                                        >
-                                            Submit & Next
-                                        </Button>
-                                    </Text>
-                                </HStack>
-                            </Form>
-                        </Box>
-                    }
-                </GridItem>
+									<Text textAlign={'end'} my={2}>
+										<Button size="lg" bg={"blue"} color={"white"} _hover={{ bg: "blue.500" }} px={4} py={1} borderRadius={'lg'} type='submit' onClick={(event) => {
+											if (input) {
+												handleSubmitNext(event);
+											}
+										}}
+										>
+											Submit & Next
+										</Button>
+									</Text>
+								</HStack>
+							</Form>
+						</Box>
+					}
+				</GridItem>
 
-                {/* right side part */}
-                <GridItem rowSpan={3} colSpan={[5, 5, 2, 2, 1]} bg='papayawhip' boxShadow='2xl' p='6' rounded='md' >
-                    <Text w="" fontSize="lg" fontWeight="bold" color='blue' textAlign={'center'} my={5}>Profile</Text>
+				{/* right side part */}
+				<GridItem rowSpan={3} colSpan={[5, 5, 2, 2, 1]} bg='papayawhip' boxShadow='2xl' p='6' rounded='md' >
+					<Text w="" fontSize="lg" fontWeight="bold" color='blue' textAlign={'center'} my={5}>Profile</Text>
 
-                    <Image borderRadius='full' boxSize='50px' mx={'auto'}
-                        src='https://bit.ly/dan-abramov' alt='Dan Abram'
-                    />
+					<Image borderRadius='full' boxSize='50px' mx={'auto'}
+						src='https://bit.ly/dan-abramov' alt='Dan Abram'
+					/>
 
-                    <Text w="" fontSize="lg" fontWeight="bold" color='black' textAlign={'center'} my={1}>dummy </Text>
+					<Text w="" fontSize="lg" fontWeight="bold" color='black' textAlign={'center'} my={1}>dummy </Text>
 
-                    <Text w="" fontSize="sm" fontWeight="bold" color='black' textAlign={'center'}
-                    >Date of Join: 22 March, 2022</Text>
-                    <Text w="" fontSize="sm" fontWeight="bold" color='black' textAlign={'center'}
-                    >Total Working Days: 356 Days</Text>
+					<Text w="" fontSize="sm" fontWeight="bold" color='black' textAlign={'center'}
+					>Date of Join: 22 March, 2022</Text>
+					<Text w="" fontSize="sm" fontWeight="bold" color='black' textAlign={'center'}
+					>Total Working Days: 356 Days</Text>
 
-                    <Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} mt={6}
-                    >Balance</Text>
-                    <Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} my={1}
-                    >::: Tk. 345.56 :::</Text>
+					<Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} mt={6}
+					>Balance</Text>
+					<Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} my={1}
+					>::: Tk. 345.56 :::</Text>
 
-                    <Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} mt={6}
-                    >For Approve</Text>
-                    <Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} my={1}
-                    >::: Tk. 35.56 :::</Text>
+					<Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} mt={6}
+					>For Approve</Text>
+					<Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} my={1}
+					>::: Tk. 35.56 :::</Text>
 
-                    <Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} mt={6}
-                    >Today's Contribution</Text>
-                    <Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} my={1}
-                    >::: Tk. 5.56 :::</Text>
-                </GridItem>
-            </Grid>
-        </Container>
-    );
+					<Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} mt={6}
+					>Today's Contribution</Text>
+					<Text w="" fontSize="lg" fontWeight="semibold" color='black' textAlign={'center'} my={1}
+					>::: Tk. 5.56 :::</Text>
+				</GridItem>
+			</Grid>
+		</Container>
+	);
 };
 
 export default Home;
