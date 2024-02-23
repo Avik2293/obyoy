d5355a2078fac22eea3b4a0e16232342e3e8fdb0
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-02-23 11:20:01 +0600

    chore: change to tabs instead of spaces for indentation

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,33 +4,32 @@ import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
 
 
 const Footer = () => {
+	return (
+		<Box bg='gray'>
+			<Container as="footer" role="contentinfo" py={{ base: '2', md: '6', }} px={{ base: '5', md: '8', }} maxWidth={"1350px"} mx={'auto'}>
+				<Stack spacing={{ base: '4', md: '5', }}>
+					<Stack justify="space-between" direction="row" align="center">
+						<Text w="100px" fontSize="lg" fontWeight="bold" color='white'>
+							Logo
+						</Text>
 
-    return (
-        <Box bg='gray'>
-            <Container as="footer" role="contentinfo" py={{ base: '2', md: '6', }} px={{ base: '5', md: '8', }} maxWidth={"1350px"} mx={'auto'}>
-                <Stack spacing={{ base: '4', md: '5', }}>
-                    <Stack justify="space-between" direction="row" align="center">
-                        <Text w="100px" fontSize="lg" fontWeight="bold" color='white'>
-                            Logo
-                        </Text>
+						<ButtonGroup variant="tertiary">
+							<IconButton as="a" href="#" aria-label="LinkedIn"
+								icon={<FaLinkedin color='white' size='20' />} />
+							<IconButton as="a" href="#" aria-label="GitHub"
+								icon={<FaGithub color='white' size='20' />} />
+							<IconButton as="a" href="#" aria-label="Twitter"
+								icon={<FaTwitter color='white' size='20' />} />
+						</ButtonGroup>
+					</Stack>
 
-                        <ButtonGroup variant="tertiary">
-                            <IconButton as="a" href="#" aria-label="LinkedIn"
-                                icon={<FaLinkedin color='white' size='20' />} />
-                            <IconButton as="a" href="#" aria-label="GitHub"
-                                icon={<FaGithub color='white' size='20' />} />
-                            <IconButton as="a" href="#" aria-label="Twitter"
-                                icon={<FaTwitter color='white' size='20' />} />
-                        </ButtonGroup>
-                    </Stack>
-
-                    <Text fontSize="sm" color="fg.subtle" fontWeight='semibold' justify="start">
-                        &copy; {new Date().getFullYear()} Chakra UI Pro, Inc. All rights reserved.
-                    </Text>
-                </Stack>
-            </Container>
-        </Box>
-    );
+					<Text fontSize="sm" color="fg.subtle" fontWeight='semibold' justify="start">
+						&copy; {new Date().getFullYear()} Aubichol Intelligent Technologies, Inc. All rights reserved.
+					</Text>
+				</Stack>
+			</Container>
+		</Box>
+	);
 };
 
 export default Footer;
