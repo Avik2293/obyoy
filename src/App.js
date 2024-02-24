b1dc5d5428b23312d4bd0a47734f0ef4309b88f8
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-02-24 13:16:45 +0600

    chore: format code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
 import { RouterProvider, } from 'react-router-dom';
 import { ChakraBaseProvider, } from '@chakra-ui/react';
-// import { store, persistor } from "./Redux/store";
 import { Provider } from 'react-redux';
 
 import { routes } from './Routes/Routes';
 import { configureStore } from './Redux/store';
 
 function App() {
-  const { store, persistor } = configureStore();
+	const { store, persistor } = configureStore();
 
-  return (
-    <Provider store={store}>
-       {/* <ChakraBaseProvider theme={theme}> */}
-      <ChakraBaseProvider>
-        {/* <div className="App"> */}
-        <RouterProvider router={routes} />
-      </ChakraBaseProvider >
-    </Provider>
-  )
+	return (
+		<Provider store={store}>
+			{/* <ChakraBaseProvider theme={theme}> */}
+			<ChakraBaseProvider>
+				<RouterProvider router={routes} />
+			</ChakraBaseProvider >
+		</Provider>
+	)
 }
 
 export default App;
