20faf840129389885355c2ce9a0498316bfd05a4
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-02-24 13:17:22 +0600

    chore: format code

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ import reportWebVitals from './reportWebVitals';
 // for mirage 
 import { makeServer } from "./server"
 if (process.env.NODE_ENV === "development") {
-  makeServer({ environment: "development" })
+	makeServer({ environment: "development" })
 }
 
 const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
+	brand: {
+		900: '#1a365d',
+		800: '#153e75',
+		700: '#2a69ac',
+	},
 }
 
 const theme = extendTheme({ colors })
