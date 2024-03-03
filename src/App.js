4272bc88affc1d6f47566d547b0071ce1c32601c
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-03-03 22:57:17 +0600

    fix: wait until the persisted state loads

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import './App.css';
 import { RouterProvider, } from 'react-router-dom';
 import { ChakraBaseProvider, } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
+import { Text } from '@chakra-ui/react'
 
 import { routes } from './Routes/Routes';
 import { configureStore } from './Redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
 	const { store, persistor } = configureStore();
 
 	return (
 		<Provider store={store}>
-			{/* <ChakraBaseProvider theme={theme}> */}
-			<ChakraBaseProvider>
-				<RouterProvider router={routes} />
-			</ChakraBaseProvider >
+			<PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+				{/* <ChakraBaseProvider theme={theme}> */}
+				<ChakraBaseProvider>
+					<RouterProvider router={routes} />
+				</ChakraBaseProvider>
+			</PersistGate>
 		</Provider>
 	)
 }
